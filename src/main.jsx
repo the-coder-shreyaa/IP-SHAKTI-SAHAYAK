3fe67909608f806459f1fde91041c805bf024c8f
the-coder-shreyaa <shreyamishra99881@gmail.com>
2026-09-22 01:30:12 +0530

    fix app import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app.jsx";
 import "./styles/global.css";
 import { LanguageProvider } from "./context/LanguageContext";
 
