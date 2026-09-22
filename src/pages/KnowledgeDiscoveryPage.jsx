36bb2af787b3d72b6db1973d62c9cf72f9692ff3
the-coder-shreyaa <shreyamishra99881@gmail.com>
2026-09-22 21:48:48 +0530

    Update KnowledgeDiscoveryPage.jsx

--- src/pages/KnowledgeDiscoveryPage.jsx
+++ src/pages/KnowledgeDiscoveryPage.jsx
@@ -12,7 +12,7 @@ import {
   ArrowRight,
 } from "lucide-react";
 
-import SectionHeader from "../components/SectionHeader";
+import SectionHeader from "../components/sectionHeader.jsx";
 import StatusBadge from "../components/StatusBadge";
 
 const knowledgeRecords = [
