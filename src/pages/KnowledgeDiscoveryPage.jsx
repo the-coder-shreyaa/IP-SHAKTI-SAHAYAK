af22d57b0151d134af50c40daed05e73efb5b4c0
the-coder-shreyaa <shreyamishra99881@gmail.com>
2026-09-25 17:33:58 +0530

    Fix SectionHeader import

--- src/pages/KnowledgeDiscoveryPage.jsx
+++ src/pages/KnowledgeDiscoveryPage.jsx
@@ -12,7 +12,7 @@ import {
   ArrowRight,
 } from "lucide-react";
 
-import SectionHeader from "../components/sectionHeader.jsx";
+import SectionHeader from "../components/SectionHeader.jsx";
 import StatusBadge from "../components/StatusBadge";
 
 const knowledgeRecords = [
